140340bcec8b86d884138a83fa0515136af29ee3
mayor01 <mayor01@users.noreply.github.com>
2019-08-28 15:18:27 +0100

    Changed my google api key

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from "redux-promise";
-
 import App from './components/app';
 import reducers from './reducers';
+
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
