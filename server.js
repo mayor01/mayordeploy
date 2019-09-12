25089538860782f5312d372fb3e9b746c3e56847
mayor01 <mayor01@users.noreply.github.com>
2019-09-12 19:45:18 +0100

    Scroll added

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express'),
-    /* cors = require('cors'), */
-    path = require('path'),
-    PORT = process.env.PORT || 8008,
-    app = express();
+const express = require("express"),
+  /* cors = require('cors'), */
+  path = require("path"),
+  PORT = process.env.PORT || 8008,
+  app = express();
 /* app.use(cors); */
 app.use(express.static(__dirname));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(PORT);
-console.log('server started');
-
+console.log("server started");
